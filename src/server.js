e582f1ce60c62e3a7e4d4c246ddf4acb0e0db1bb
Sumit singh <61839226+sumitsingh0003@users.noreply.github.com>
2026-02-02 15:06:15 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2100;
 
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
